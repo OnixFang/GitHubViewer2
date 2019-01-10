d510c59837881a536daa6e1de12269219391fef2
Ulises Taveras <twily.dev@outlook.com>
2019-01-10 18:55:58 -0400

    correct index path

--- scritps/server.js
+++ scritps/server.js
@@ -5,7 +5,7 @@ var rootPath = path.normalize(__dirname + '/../');
 
 app.use(express.static(rootPath + '/dist'));
 
-app.get('*', (req, res) => { res.sendFile(rootPath + '/dist/index.html'); });
+app.get('*', (req, res) => { res.sendFile(rootPath + '/dist/GitHubViewer2/index.html'); });
 
 app.listen(8010);
 console.log('Listening on port 8010...');
